c92833c894c33b6245d7ff95a0de15ff2e014719
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-07 17:45:50 +0500

    Task3

---
+++ Tyuiu.chernyhim.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.chernyhim.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.chernyhim.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.chernyhim.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
