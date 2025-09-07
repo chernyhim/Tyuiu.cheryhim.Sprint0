783c1073727b6f9745154bb1e36506b29ffbeca5
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-07 13:03:28 +0500

    Task1

---
+++ Tyuiu.cheryhim.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "John";
+        int age = 30;
+        Console.WriteLine("My name is " + name + " and I am " + age + " years old.");
+        Console.ReadKey();
+    }
+}
