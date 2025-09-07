e8290c220e99d721b542f340d1cb2ba6185f770e
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-07 19:47:35 +0500

    Task6

---
+++ Tyuiu.chernyhim.Sprint.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.chernyhim.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.chernyhim.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res=DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckedSubstructionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstructionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckedMultiArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.chernyhim.Sprint0.Task6.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Sum of array elements: " + DataService.AdditionArray(numsArray));
+        Console.WriteLine("Subtraction of array elements: " + DataService.SubstructionArray(numsArray));
+        Console.WriteLine("Multiplication of array elements: " + DataService.MultiplicationArray(numsArray));
+        Console.ReadKey();
+    }
+}
