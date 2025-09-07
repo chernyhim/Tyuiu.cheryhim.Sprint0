183f6b3fcd552f094d77013f17edf3f5855cb0c3
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-07 19:05:43 +0500

    Task5

---
+++ Tyuiu.chernyhim.Sprint0.Task5.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿namespace Tyuiu.chernyhim.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Denominator cannot be zero.");
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
